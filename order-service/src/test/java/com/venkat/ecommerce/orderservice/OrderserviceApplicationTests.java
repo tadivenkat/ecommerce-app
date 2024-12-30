22f44d5b4abbb2e8e6b2fab8b169cecb67de46af
Venkat Ram Taddi <tadivenkat@gmail.com>
2024-12-29 20:11:26 -0600

    Implemented config-server, discovery-service and a skeleton of order-service

---
+++ order-service/src/test/java/com/venkat/ecommerce/orderservice/OrderserviceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.venkat.ecommerce.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class OrderserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
